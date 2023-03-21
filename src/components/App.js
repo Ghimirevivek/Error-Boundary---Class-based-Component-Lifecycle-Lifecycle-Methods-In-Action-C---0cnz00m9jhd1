cce905381a508021f15c61de86a283a7d9078244
Ghimirevivek <stephengmre@gmail.com>
2023-03-21 13:11:51 +0530

    m

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css'
 import Component from './Component'
 import ErrorBoundary from './ErrorBoundary'
 const App = () => {
+  const [error, setError] = useState(false)
+
   const errorGenerator = () => {
-    throw new Error('Uncaught Exception Generated')
+    setError(true)
   }
   const changeFunc = () => {
-    // change function implementation
+    console.log(error)
   }
   return (
     <div id="main">
       <h2>React Error Boundaries</h2>
       <ErrorBoundary>
-        <Component change={changeFunc} />
+        <Component change={changeFunc} error={error} />
       </ErrorBoundary>
       <button id="gen" onClick={errorGenerator}>
         Generate Error
--- src/components/Component.js
+++ src/components/Component.js
@@ -1,16 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-const Component = (props) => {
-  const [error, setError] = useState(false)
+const Component = ({ change, error }) => {
   if (error) {
     throw new Error('Uncaught Exception')
   }
-  return (
-    <>
-      {error && <div>{somethingNotDefined}</div>}
-      <button onClick={() => setError(true)}>Throw Error</button>
-    </>
-  )
+  return <>{error && <div>{change()}</div>}</>
 }
 
 export default Component
